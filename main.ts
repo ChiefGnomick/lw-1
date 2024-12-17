0d9ab17501e05787fb95f2d976e07734f3bf381f
ChiefGnomick <163769071+ChiefGnomick@users.noreply.github.com>
2024-12-17 17:42:38 +0400

    first commit

---
+++ main.ts
@@ -0,0 +1,22 @@
+const concatenateNumbers = (number1, number2) => `${number1}${number2}`;
+console.log(concatenateNumbers(52, 25)); 
+
+const constantNumber = 52;
+const constantString = "Hello, World!";
+const constantBoolean = true;
+
+let variableArray = [1, 2, 3];
+let variableObject = { key: "value" };
+
+const constantNull = null;
+let variableUndefined;
+
+const constantBigInt = 123456789012345678901234567890n;
+
+const data = {
+  id: 1,
+  name: "Василий",
+};
+
+const jsonString = JSON.stringify(data);
+console.log(jsonString);
